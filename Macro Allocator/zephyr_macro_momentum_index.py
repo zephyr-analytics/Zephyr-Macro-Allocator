1d6a614489c28c8bfa852ddcffc8257efd6b5c0a
Monica Claire W. <101022450+monicacw21@users.noreply.github.com>
2026-02-21 13:08:24 -0800

    -Update

--- Macro Allocator/zephyr_macro_momentum_index.py
+++ Macro Allocator/zephyr_macro_momentum_index.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 import numpy as np
 import pandas as pd
 
+
 class ZephyrMacroAllocator(QCAlgorithm):
     """
     A multi-asset macro allocation algorithm that uses dual-momentum, 
